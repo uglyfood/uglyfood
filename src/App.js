837cf77c4568ae011ba7d3da6ea89d9293f77a1c
devxmachina <alfago803@gmail.com>
2022-05-13 22:12:47 +0900

    First Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './media/logo.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          INGREDIENTS THAT ARE ABANDONED. WHY?
+        </p>
+        <a
+          className="App-link"
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn More
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
